436b59f7cfa7310f7bbfbdd73d0274b923b00dec
Luka Pervan <lukapervan@hotmail.com>
2023-01-07 22:10:14 +0100

    Crypto watcher
    Crypto app with API and Bloc Provider

---
+++ crypto_app/lib/services/data_repo.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:crypto_app/model/currency.dart';
+import 'package:crypto_app/model/data_error.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class CryptoRepository {
+  static const String _baseUrl = 'https://min-api.cryptocompare.com/';
+  final http.Client _httpClient;
+
+  CryptoRepository({http.Client? httpClient})
+      : _httpClient = httpClient ?? http.Client();
+
+  Future<List<CryptoCurrency>> getCurrencies() async {
+    const requestUrl =
+        '${_baseUrl}data/top/totalvolfull?limit=25&tsym=USD&page=0';
+
+    try {
+      final response = await _httpClient.get(Uri.parse(requestUrl));
+
+      if(response.statusCode == 200) {
+        Map<String, dynamic> data = json.decode(response.body);
+        final coinList = List.from(data['Data']);
+        return coinList.map((e) => CryptoCurrency.fromMap(e)).toList();
+      }
+      return [];
+    } catch (err) {
+      throw DataError(message: err.toString());
+    }
+  }
+}
